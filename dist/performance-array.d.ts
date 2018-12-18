a059ff59669060afd117c0e254d992c1b21b3df8
JMuratha <jan@muratha.at>
2018-12-18 22:15:07 +0100

    updated dependencies

--- dist/performance-array.d.ts
+++ dist/performance-array.d.ts
@@ -1,12 +1,23 @@
 declare namespace PerformanceArray {
+    interface IPerformanceArrayIndexOptions {
+        propertyNames: Array<string>;
+    }
     interface IPerformanceArrayOptions {
+        indices?: Array<IPerformanceArrayIndexOptions>;
     }
 }
 declare namespace PerformanceArray {
     class PerformanceArray<T> {
         private _arrayData;
-        private _options;
-        constructor(arrayData: Array<T>, options?: IPerformanceArrayOptions);
+        constructor(arrayData: Array<T>);
+        item(i: number): T;
+        remove(item: T): void;
+        push(item: T): void;
+        pop(): T | undefined;
+        unshift(item: T): void;
+        shift(): T | undefined;
+        splice(index: number, deleteCount: number, ...insertItems: Array<T>): Array<T>;
         toArray(): Array<T>;
+        readonly length: number;
     }
 }
--- dist/performance-array.js
+++ dist/performance-array.js
@@ -1,14 +1,47 @@
-/// <reference path="./IPerformanceArrayOptions.ts" />
 var PerformanceArray;
 (function (PerformanceArray_1) {
-    var PerformanceArray = /** @class */ (function () {
-        function PerformanceArray(arrayData, options) {
+    var PerformanceArray = (function () {
+        function PerformanceArray(arrayData) {
             this._arrayData = arrayData;
-            this._options = options ? options : {};
         }
+        PerformanceArray.prototype.item = function (i) {
+            return this._arrayData[i];
+        };
+        PerformanceArray.prototype.remove = function (item) {
+            var index = this._arrayData.indexOf(item);
+            if (index >= 0) {
+                this._arrayData.splice(index, 1);
+            }
+        };
+        PerformanceArray.prototype.push = function (item) {
+            this._arrayData.push(item);
+        };
+        PerformanceArray.prototype.pop = function () {
+            return this._arrayData.pop();
+        };
+        PerformanceArray.prototype.unshift = function (item) {
+            this._arrayData.unshift(item);
+        };
+        PerformanceArray.prototype.shift = function () {
+            return this._arrayData.shift();
+        };
+        PerformanceArray.prototype.splice = function (index, deleteCount) {
+            var insertItems = [];
+            for (var _i = 2; _i < arguments.length; _i++) {
+                insertItems[_i - 2] = arguments[_i];
+            }
+            return this._arrayData.splice.apply(this._arrayData, [index, deleteCount].concat(insertItems));
+        };
         PerformanceArray.prototype.toArray = function () {
             return this._arrayData.slice();
         };
+        Object.defineProperty(PerformanceArray.prototype, "length", {
+            get: function () {
+                return this._arrayData.length;
+            },
+            enumerable: true,
+            configurable: true
+        });
         return PerformanceArray;
     }());
     PerformanceArray_1.PerformanceArray = PerformanceArray;
